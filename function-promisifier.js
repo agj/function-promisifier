0af7630b9f1b36bf31db128ab3fd61f87f2871aa
agj <ale@agj.cl>
2015-11-18 02:55:01 +0900

    Making sure that all arguments are passed to target function, regardless of arity.

--- function-promisifier.js
+++ function-promisifier.js
@@ -1,11 +1,27 @@
 'use strict';
 
-var nAry = require('ramda/src/nAry');
 var apply = require('ramda/src/apply');
 var toArray = Function.prototype.call.bind([].slice);
 
+function ary(n, fn) {
+	switch (n) {
+		case 0: return function() { return fn.apply(this, toArray(arguments)) };
+		case 1: return function(a0) { return fn.apply(this, toArray(arguments)) };
+		case 2: return function(a0, a1) { return fn.apply(this, toArray(arguments)) };
+		case 3: return function(a0, a1, a2) { return fn.apply(this, toArray(arguments)) };
+		case 4: return function(a0, a1, a2, a3) { return fn.apply(this, toArray(arguments)) };
+		case 5: return function(a0, a1, a2, a3, a4) { return fn.apply(this, toArray(arguments)) };
+		case 6: return function(a0, a1, a2, a3, a4, a5) { return fn.apply(this, toArray(arguments)) };
+		case 7: return function(a0, a1, a2, a3, a4, a5, a6) { return fn.apply(this, toArray(arguments)) };
+		case 8: return function(a0, a1, a2, a3, a4, a5, a6, a7) { return fn.apply(this, toArray(arguments)) };
+		case 9: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8) { return fn.apply(this, toArray(arguments)) };
+		case 10: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9) { return fn.apply(this, toArray(arguments)) };
+		default: throw new Error("Arity should be a number between 0 and 10.");
+	}
+}
+
 var promisifierN = function (arity, fn) {
-	return nAry(arity, function () {
+	return ary(arity, function () {
 		return Promise.all(toArray(arguments))
 			.then(apply(fn.bind(this)));
 	});
--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ global.Promise = require('pinkie-promise');
 
 var promisifier = require('../');
 var test = require('tape-catch');
+var toArray = Function.prototype.call.bind([].slice);
 
 function add(a, b) {
 	return a + b;
@@ -52,5 +53,22 @@ test("Specifying arity.", function (assert) {
 
 	assert.equal(2, promisifier(add).length);
 	assert.equal(5, promisifier.n(5, add).length);
-})
+});
+
+test("Regardless of function arity, all arguments should be passed to target function.", function (assert) {
+	assert.plan(2);
+	var returnArgumentsP = promisifier(function returnArgumentsP(a, b) {
+		return toArray(arguments);
+	});
+
+	returnArgumentsP(1, 2)
+	.then( function (result) {
+		assert.deepEqual(result, [1, 2]);
+	});
+
+	returnArgumentsP(1, 2, 3, 4, 5)
+	.then( function (result) {
+		assert.deepEqual(result, [1, 2, 3, 4, 5]);
+	});
+});
 
